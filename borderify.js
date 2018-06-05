17850087ff9dd02ed621e860defe4902d332d249
LAXMANN A/L RAVINDRAN <laxmannturbo@gmail.com>
2018-06-05 15:37:04 +0800

    added a code - Laxmann
    changes made by Laxmann.

--- borderify.js
+++ borderify.js
@@ -2,4 +2,4 @@
 Just draw a border round the document.body.
 */
 document.body.style.border = "5px solid red";
-document.body.style.fontSize = "xx-large";
+document.body.style.fontSize = "xx-large"; //changes Made by Laxmann
